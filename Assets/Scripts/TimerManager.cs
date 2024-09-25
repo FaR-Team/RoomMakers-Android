678b7135be6621448a64ee806c478d4dd1786fbf
Santiago Fisela <santyfisela@gmail.com>
2024-09-25 20:58:39 -0300

    Timer reduced to half + flicking.

--- Assets/Scripts/TimerManager.cs
+++ Assets/Scripts/TimerManager.cs
@@ -3,14 +3,16 @@ using UnityEngine;
 
 public class TimerManager : MonoBehaviour
 {
-    private static float maxTime = 300f;
+    private static float maxTime = 150f;
     private static float time;
-
     [SerializeField] private SpriteRenderer clock;
     [SerializeField] Sprite[] clockSprites;
 
     public static Action timerAction;
 
+    [SerializeField] private float flickerInterval = 0.5f;
+    private float flickerTimer = 0f;
+    private bool isVisible = true;
     private void Start()
     {
         time = maxTime;
@@ -21,23 +23,40 @@ public class TimerManager : MonoBehaviour
     {
         timerAction?.Invoke();
 
-        if (time > 240)
+        if (time > 120)
         {
             clock.sprite = clockSprites[0];
         }
-        else if (time > 180)
+        else if (time > 90)
         {
             clock.sprite = clockSprites[1];
         }
-        else if (time > 120)
+        else if (time > 60)
         {
             clock.sprite = clockSprites[2];
         }
-        else if (time > 60)
+        else if (time > 30)
         {
             clock.sprite = clockSprites[3];
         }
-        else clock.sprite = clockSprites[4];
+        else
+        {
+            clock.sprite = clockSprites[4];
+            FlickerClock();
+        }
+    }
+
+    private void FlickerClock()
+    {
+        flickerTimer += Time.deltaTime;
+        if (flickerTimer >= flickerInterval)
+        {
+            isVisible = !isVisible;
+            Color clockColor = clock.color;
+            clockColor.a = isVisible ? 1f : 0f;
+            clock.color = clockColor;
+            flickerTimer = 0f;
+        }
     }
 
     public static void Timer()
