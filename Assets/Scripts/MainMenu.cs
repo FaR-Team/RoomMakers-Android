02c1f59dcbf94f76eac3c9a48e8de64f5fc28d1e
Santiago Fisela <santyfisela@gmail.com>
2025-06-05 18:29:35 -0300

    Main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -8,52 +9,223 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] private Animator blackFade;
     [SerializeField] private SpriteRenderer controles;
+    [SerializeField] private SpriteRenderer credits;
     [SerializeField] private GameObject menuPanel;
     [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject gameModePanel;
     [SerializeField] private Button playButton;
     [SerializeField] private Button controlsButton;
     [SerializeField] private Button leaderboardsButton;
     [SerializeField] private Button creditsButton;
-
+    [SerializeField] private Button CasualButton;
+    [SerializeField] private Button RogueButton;
+    private Controls controls;
+    private Button[] menuButtons;
+    private Button[] gameModeButtons;
+    private Button[] currentButtons;
+    private int currentButtonIndex = 0;
     private int value = 0;
     bool IsAlreadyChangingScene;
+    private bool controlsOpen = false;
+    private bool creditsOpen = false;
+    private bool gameModeMenuOpen = false;
+    
+    void Awake()
+    {
+        controls = new Controls();
+        menuButtons = new Button[] { playButton, controlsButton, leaderboardsButton, creditsButton };
+        gameModeButtons = new Button[] { CasualButton, RogueButton };
+        currentButtons = menuButtons;
+    }
+
+    void OnEnable()
+    {
+        controls.Enable();
+        controls.Movement.Movement.performed += OnNavigate;
+        controls.Movement.Interact.performed += OnInteract;
+    }
+
+    void OnDisable()
+    {
+        controls.Movement.Movement.performed -= OnNavigate;
+        controls.Movement.Interact.performed -= OnInteract;
+        controls.Disable();
+    }
     
     void Start()
     {
         menuPanel.SetActive(false);
+        gameModePanel.SetActive(false);
         SetupButtons();
+        SetupNavigation();
     }
 
     void Update()
     {
-        if (Input.anyKeyDown && !IsAlreadyChangingScene && value < 2)
+        if (Input.anyKeyDown && !IsAlreadyChangingScene)
         {
-            value++;
-            switch (value)
+            if (controlsOpen)
             {
-                case 0:
-                    ActivateControllerScreen(false);
-                    break;
+                StartCoroutine(CloseControls());
+                return;
+            }
+            
+            if (creditsOpen)
+            {
+                StartCoroutine(CloseCredits());
+                return;
+            }
 
-                case 1:
-                    ActivateControllerScreen(true);
-                    AudioManager.instance.PlaySfx(GlobalSfx.Click);
-                    break;
-                default:
-                    AudioManager.instance.PlaySfx(GlobalSfx.Grab);
-                    blackFade.SetTrigger("StartBlackFade");
-                    Invoke("FadeToMenu", 0.5f);
-                    break;
+            /*if (gameModeMenuOpen)
+            {
+                BackToMainMenu();
+                return;
+            }*/
+            
+            if (value < 2)
+            {
+                value++;
+                switch (value)
+                {
+                    case 0:
+                        ActivateControllerScreen(false);
+                        break;
+
+                    case 1:
+                        ActivateControllerScreen(true);
+                        AudioManager.instance.PlaySfx(GlobalSfx.Click);
+                        break;
+                    default:
+                        AudioManager.instance.PlaySfx(GlobalSfx.Grab);
+                        blackFade.SetTrigger("StartBlackFade");
+                        Invoke("FadeToMenu", 0.5f);
+                        break;
+                }
+            }
+        }
+    }
+
+    private void OnNavigate(InputAction.CallbackContext context)
+    {
+        if ((!menuPanel.activeInHierarchy && !gameModePanel.activeInHierarchy) || controlsOpen || creditsOpen) return;
+
+        Vector2 input = context.ReadValue<Vector2>();
+        
+        if (input.y > 0.5f)
+        {
+            NavigateUp();
+        }
+        else if (input.y < -0.5f)
+        {
+            NavigateDown();
+        }
+    }
+
+    private void OnInteract(InputAction.CallbackContext context)
+    {
+        if ((!menuPanel.activeInHierarchy && !gameModePanel.activeInHierarchy) || controlsOpen || creditsOpen) return;
+        
+        currentButtons[currentButtonIndex].onClick.Invoke();
+    }
+
+    private void NavigateUp()
+    {
+        currentButtonIndex = (currentButtonIndex - 1 + currentButtons.Length) % currentButtons.Length;
+        UpdateButtonSelection();
+        AudioManager.instance.PlaySfx(GlobalSfx.Click);
+    }
+
+    private void NavigateDown()
+    {
+        currentButtonIndex = (currentButtonIndex + 1) % currentButtons.Length;
+        UpdateButtonSelection();
+        AudioManager.instance.PlaySfx(GlobalSfx.Click);
+    }
+
+    private void UpdateButtonSelection()
+    {
+        for (int i = 0; i < currentButtons.Length; i++)
+        {
+            Image buttonImage = currentButtons[i].GetComponent<Image>();
+            
+            if (i == currentButtonIndex)
+            {
+                if (buttonImage != null)
+                {
+                    Color imageColor = buttonImage.color;
+                    imageColor.a = 1f;
+                    buttonImage.color = imageColor;
+                }
+                currentButtons[i].Select();
+            }
+            else
+            {
+                if (buttonImage != null)
+                {
+                    Color imageColor = buttonImage.color;
+                    imageColor.a = 0.5f;
+                    buttonImage.color = imageColor;
+                }
             }
         }
     }
 
     private void SetupButtons()
     {
-        playButton.onClick.AddListener(() => LoadScene(2));
+        playButton.onClick.AddListener(ShowGameModeMenu);
         controlsButton.onClick.AddListener(ShowControls);
-        leaderboardsButton.onClick.AddListener(ShowLeaderboards);
+        leaderboardsButton.onClick.AddListener(() => ShakeButtonWithError(leaderboardsButton));
         creditsButton.onClick.AddListener(ShowCredits);
+        
+        CasualButton.onClick.AddListener(() => LoadScene(2));
+        RogueButton.onClick.AddListener(() => ShakeButtonWithError(RogueButton));
+    }
+
+    private void ShakeButtonWithError(Button button)
+    {
+        AudioManager.instance.PlaySfx(GlobalSfx.Error);
+        StartCoroutine(ShakeButton(button));
+    }
+
+    private IEnumerator ShakeButton(Button button)
+    {
+        Vector3 originalPosition = button.transform.localPosition;
+        float shakeDuration = 0.3f;
+        float shakeIntensity = 0.05f;
+        float elapsed = 0f;
+
+        while (elapsed < shakeDuration)
+        {
+            float x = originalPosition.x + Random.Range(-shakeIntensity, shakeIntensity);
+            float y = originalPosition.y + Random.Range(-shakeIntensity, shakeIntensity);
+            
+            button.transform.localPosition = new Vector3(x, y, originalPosition.z);
+            
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        button.transform.localPosition = originalPosition;
+    }
+
+    private void SetupNavigation()
+    {
+        SetupButtonNavigation(menuButtons);
+        SetupButtonNavigation(gameModeButtons);
+    }
+
+    private void SetupButtonNavigation(Button[] buttons)
+    {
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            Navigation nav = new Navigation();
+            nav.mode = Navigation.Mode.Explicit;
+            
+            nav.selectOnUp = buttons[(i - 1 + buttons.Length) % buttons.Length];
+            nav.selectOnDown = buttons[(i + 1) % buttons.Length];
+            
+            buttons[i].navigation = nav;
+        }
     }
 
     private void FadeToMenu()
@@ -67,6 +239,42 @@ public class MainMenu : MonoBehaviour
     private void ShowMainMenu()
     {
         menuPanel.SetActive(true);
+        gameModePanel.SetActive(false);
+        currentButtons = menuButtons;
+        currentButtonIndex = 0;
+        gameModeMenuOpen = false;
+        UpdateButtonSelection();
+        playButton.Select();
+    }
+
+    private void ShowGameModeMenu()
+    {
+        StartCoroutine(GameModeMenuData());
+    }
+
+    private IEnumerator GameModeMenuData()
+    {
+        AudioManager.instance.PlaySfx(GlobalSfx.Click);
+        menuPanel.SetActive(false);
+        gameModePanel.SetActive(true);
+        currentButtons = gameModeButtons;
+        currentButtonIndex = 0;
+        gameModeMenuOpen = true;
+        yield return null;
+        UpdateButtonSelection();
+        CasualButton.Select();
+    }
+
+    private void BackToMainMenu()
+    {
+        AudioManager.instance.PlaySfx(GlobalSfx.Grab);
+        gameModePanel.SetActive(false);
+        menuPanel.SetActive(true);
+        currentButtons = menuButtons;
+        currentButtonIndex = 0;
+        gameModeMenuOpen = false;
+        UpdateButtonSelection();
+        playButton.Select();
     }
 
     private void LoadScene(int sceneIndex)
@@ -90,20 +298,50 @@ public class MainMenu : MonoBehaviour
     {
         AudioManager.instance.PlaySfx(GlobalSfx.Click);
         ActivateControllerScreen(true);
+        menuPanel.SetActive(false);
+        controlsOpen = true;
+    }
+
+    private IEnumerator CloseControls()
+    {
+        AudioManager.instance.PlaySfx(GlobalSfx.Grab);
+        ActivateControllerScreen(false);
+        yield return null;
+        menuPanel.SetActive(true);
+        controlsOpen = false;
+        UpdateButtonSelection();
     }
 
     private void ShowLeaderboards()
     {
-        AudioManager.instance.PlaySfx(GlobalSfx.Click);
+        AudioManager.instance.PlaySfx(GlobalSfx.Error);
     }
 
     private void ShowCredits()
     {
         AudioManager.instance.PlaySfx(GlobalSfx.Click);
+        ActivateCreditsScreen(true);
+        menuPanel.SetActive(false);
+        creditsOpen = true;
+    }
+
+    private IEnumerator CloseCredits()
+    {
+        AudioManager.instance.PlaySfx(GlobalSfx.Grab);
+        ActivateCreditsScreen(false);
+        yield return null;
+        menuPanel.SetActive(true);
+        creditsOpen = false;
+        UpdateButtonSelection();
     }
 
     private void ActivateControllerScreen(bool setActive)
     {
         controles.enabled = setActive;
+    }
+
+    private void ActivateCreditsScreen(bool setActive)
+    {
+        credits.enabled = setActive;
     }
 }
--- Assets/Scripts/MainMenuLocalizationManager.cs
+++ Assets/Scripts/MainMenuLocalizationManager.cs
@@ -1,40 +1,67 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuLocalizationManager : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer controlsScreenRenderer;
     [SerializeField] private Sprite englishControlsSprite;
     [SerializeField] private Sprite spanishControlsSprite;
+    
+    [SerializeField] private SpriteRenderer creditsScreenRenderer;
+    [SerializeField] private Sprite englishCreditsSprite;
+    [SerializeField] private Sprite spanishCreditsSprite;
+    
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button controlsButton;
+    [SerializeField] private Button leaderboardsButton;
+    [SerializeField] private Button creditsButton;
+    [SerializeField] private Button casualButton;
+    [SerializeField] private Button rogueButton;
+    
+    [SerializeField] private Sprite englishPlaySprite;
+    [SerializeField] private Sprite spanishPlaySprite;
+    [SerializeField] private Sprite englishControlsButtonSprite;
+    [SerializeField] private Sprite spanishControlsButtonSprite;
+    [SerializeField] private Sprite englishLeaderboardsSprite;
+    [SerializeField] private Sprite spanishLeaderboardsSprite;
+    [SerializeField] private Sprite englishCreditsButtonSprite;
+    [SerializeField] private Sprite spanishCreditsButtonSprite;
+    [SerializeField] private Sprite englishCasualSprite;
+    [SerializeField] private Sprite spanishCasualSprite;
+    [SerializeField] private Sprite englishRogueSprite;
+    [SerializeField] private Sprite spanishRogueSprite;
 
     private bool isSpanish;
 
     private void Start()
     {
-        // Check if the main LocalizationManager exists and get language preference
         if (LocalizationManager.Instance != null)
         {
             isSpanish = LocalizationManager.Instance.IsSpanish;
         }
         else
         {
-            // Fallback to system language detection if the main manager isn't available
             DetectLanguage();
         }
 
-        // Apply the appropriate sprite
-        UpdateControlsSprite();
+        UpdateAllSprites();
     }
 
     private void DetectLanguage()
     {
-        // Get the system language
         SystemLanguage deviceLanguage = Application.systemLanguage;
         
-        // Check if the language is Spanish or a Spanish variant
         isSpanish = deviceLanguage == SystemLanguage.Spanish || 
                    deviceLanguage == SystemLanguage.Catalan;
         
         Debug.Log($"Main Menu - Device language detected: {deviceLanguage}. Spanish mode: {isSpanish}");
+    }
+
+    private void UpdateAllSprites()
+    {
+        UpdateControlsSprite();
+        UpdateCreditsSprite();
+        UpdateButtonSprites();
     }
 
     private void UpdateControlsSprite()
@@ -51,15 +78,56 @@ public class MainMenuLocalizationManager : MonoBehaviour
             return;
         }
 
-        // Set the appropriate sprite based on language
         controlsScreenRenderer.sprite = isSpanish ? spanishControlsSprite : englishControlsSprite;
         Debug.Log($"Updated controls screen to {(isSpanish ? "Spanish" : "English")} version");
     }
 
-    // Public method to manually update the controls sprite
+    private void UpdateCreditsSprite()
+    {
+        if (creditsScreenRenderer == null)
+        {
+            Debug.LogError("Credits Screen Renderer not assigned in MainMenuLocalizationManager!");
+            return;
+        }
+
+        if (englishCreditsSprite == null || spanishCreditsSprite == null)
+        {
+            Debug.LogError("Credits sprites not assigned in MainMenuLocalizationManager!");
+            return;
+        }
+
+        creditsScreenRenderer.sprite = isSpanish ? spanishCreditsSprite : englishCreditsSprite;
+    }
+
+    private void UpdateButtonSprites()
+    {
+        UpdateButtonSprite(playButton, englishPlaySprite, spanishPlaySprite);
+        UpdateButtonSprite(controlsButton, englishControlsButtonSprite, spanishControlsButtonSprite);
+        UpdateButtonSprite(leaderboardsButton, englishLeaderboardsSprite, spanishLeaderboardsSprite);
+        UpdateButtonSprite(creditsButton, englishCreditsButtonSprite, spanishCreditsButtonSprite);
+        UpdateButtonSprite(casualButton, englishCasualSprite, spanishCasualSprite);
+        UpdateButtonSprite(rogueButton, englishRogueSprite, spanishRogueSprite);
+    }
+
+    private void UpdateButtonSprite(Button button, Sprite englishSprite, Sprite spanishSprite)
+    {
+        if (button == null) return;
+
+        Image buttonImage = button.GetComponent<Image>();
+        if (buttonImage == null) return;
+
+        if (englishSprite == null || spanishSprite == null)
+        {
+            Debug.LogError($"Button sprites not assigned for {button.name} in MainMenuLocalizationManager!");
+            return;
+        }
+
+        buttonImage.sprite = isSpanish ? spanishSprite : englishSprite;
+    }
+
     public void SetLanguage(bool spanish)
     {
         isSpanish = spanish;
-        UpdateControlsSprite();
+        UpdateAllSprites();
     }
 }
